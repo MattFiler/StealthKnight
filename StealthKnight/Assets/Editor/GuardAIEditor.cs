0634569c91a6010d6cf846f3c7ebf896fbb34550
Toby Jones <Toby>
2019-11-22 16:13:39 +0000

    Guard AI

---
+++ StealthKnight/Assets/Editor/GuardAIEditor.cs
@@ -0,0 +1,34 @@
+﻿// Name this script "EffectRadiusEditor"
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GuardAI))]
+public class EnemyAIEditor : Editor
+{
+    public void OnSceneGUI()
+    {
+        GuardAI t = (target as GuardAI);
+
+        EditorGUI.BeginChangeCheck();
+
+        Vector3[] waypoints = new Vector3[t.navPoints.Length + 1];
+        for (int i = 0; i < t.navPoints.Length; i++)
+        {
+            waypoints[i] = Handles.FreeMoveHandle(t.navPoints[i], Quaternion.identity, 0.1f, Vector3.one, Handles.CircleHandleCap);
+            if (i > 0 && i < t.navPoints.Length && t.navPoints.Length > 1)
+            {
+                Handles.DrawLine(waypoints[i], waypoints[i - 1]);
+            }
+        }
+        Handles.DrawLine(t.gameObject.transform.position, waypoints[0]);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Changed guard AI Settings");
+
+            for (int i = 0; i < t.navPoints.Length; i++)
+            {
+                t.navPoints[i] = waypoints[i];
+            }
+        }
+    }
+}
---
+++ StealthKnight/Assets/Scripts/GuardAI.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardAI : MonoBehaviour
+{
+    public Vector3[] navPoints;
+    [SerializeField] private navType navigationType;
+    [SerializeField] private GameObject player;
+
+    private int currentNavIndex = 0;
+    private bool navLoopForward = true;
+
+    private NavMeshAgent agent;
+    private bool fixate = false;
+
+    public enum navType
+    {
+        inOrderLoop = 0, // Ai will navigate to the first, second, ..., last position in navPoints then start at first again
+        inOrderReverse = 1, // Ai will navigate to the first, second, ..., last position in navPoints then go in reverse
+        random = 2, // Ai will select a rnadom nav point each time
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //FixatePlayer(true);
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = navPoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!fixate && Vector3.Distance(transform.position, navPoints[currentNavIndex]) < 2)
+        {
+            GetNextNavPoint();
+            agent.destination = navPoints[currentNavIndex];
+        }
+    }
+
+    void FixatePlayer(bool shouldFixate)
+    {
+        fixate = shouldFixate;
+        if(fixate)
+        {
+            agent.destination = player.transform.position;
+        }
+    }
+
+    private void GetNextNavPoint()
+    {
+        if (navPoints.Length < 2)
+            return;
+
+        switch (navigationType)
+        {
+            case navType.inOrderLoop:
+                {
+                    currentNavIndex++;
+                    if (currentNavIndex == navPoints.Length)
+                        currentNavIndex = 0;
+                    break;
+                }
+            case navType.inOrderReverse:
+                {
+                    if (navLoopForward)
+                    {
+                        currentNavIndex++;
+                        if (currentNavIndex == navPoints.Length)
+                        {
+                            currentNavIndex -= 2;
+                            navLoopForward = false;
+                        }
+                    }
+                    else
+                    {
+                        currentNavIndex--;
+                        if (currentNavIndex == -1)
+                        {
+                            currentNavIndex = 1;
+                            navLoopForward = true;
+                        }
+                    }
+                    break;
+                }
+            case navType.random:
+                {
+                    currentNavIndex = Random.Range(0, navPoints.Length);
+                    break;
+                }
+        }
+    }
+}
