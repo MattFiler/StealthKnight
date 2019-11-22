bc0d309a6f31f4dc1621aac55e0641636e532652
Evan Diamond <evan2.diamond@live.uwe.ac.uk>
2019-11-22 17:42:55 +0000

    Improved movement (now uses rigid bodies)

--- StealthKnight/Assets/PlayerMovement.cs
+++ StealthKnight/Assets/PlayerMovement.cs
@@ -4,33 +4,89 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 0.0f;
-    [SerializeField] float rotationSpeed = 0.0f;
+    [SerializeField] private float moveSpeed = 0.0f;
+    [SerializeField] private float rotationSpeed = 0.0f;
+                      
+    [SerializeField] private Animator knightAnimator;
 
-    [SerializeField] Animator knightAnimator;
-
+    [SerializeField] private float maxVelocityMagnitude = 0.0f;
+    private float maxVelocityMagnitudeSqrd = 0.0f;
+    private Vector3 velocity = Vector3.zero;
     void Start()
     {
-
+        //Make it magnitude squared
+        maxVelocityMagnitudeSqrd = Mathf.Pow(maxVelocityMagnitude,2);
     }
 
     void FixedUpdate()
     {
-        float x = moveSpeed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
-        float z = moveSpeed * Input.GetAxis("Vertical") * Time.fixedDeltaTime;
-        Vector3 dir = new Vector3(x, 0, z);
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            velocity.x += moveSpeed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
+        }
+        else
+        {
+            if(velocity.x > 0)
+            {
+                velocity.x -= moveSpeed * Time.fixedDeltaTime;
+                if (velocity.x < 0)
+                    velocity.x = 0;
+            }
+            else if(velocity.x < 0)
+            {
+                velocity.x += moveSpeed * Time.fixedDeltaTime;
+                if (velocity.x > 0)
+                    velocity.x = 0;
+            }
+        }
 
-        transform.position += dir;
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            velocity.z += moveSpeed * Input.GetAxis("Vertical") * Time.fixedDeltaTime;
+        }
+        else
+        {
+            if (velocity.z > 0)
+            {
+                velocity.z -= moveSpeed * Time.fixedDeltaTime;
+                if (velocity.z < 0)
+                    velocity.z = 0;
+            }
+            else if (velocity.z < 0)
+            {
+                velocity.z += moveSpeed * Time.fixedDeltaTime;
+                if (velocity.z > 0)
+                    velocity.z = 0;
+            }
+        }
 
-        if (dir != Vector3.zero)
+        if (velocity.sqrMagnitude > maxVelocityMagnitudeSqrd)
+        {
+            velocity.Normalize();
+            velocity *= maxVelocityMagnitude;
+        }
+
+        knightAnimator.SetFloat("Walk Speed", velocity.magnitude);
+       
+        velocity.y = GetComponent<Rigidbody>().velocity.y;
+        GetComponent<Rigidbody>().velocity = velocity;
+
+        if (!(velocity.x == 0 && velocity.z == 0))
         {
             transform.rotation = Quaternion.Slerp(
                 transform.rotation,
-                Quaternion.LookRotation(dir),
-                Time.deltaTime * rotationSpeed
+                Quaternion.LookRotation(velocity),
+                Time.fixedDeltaTime * rotationSpeed
             );
         }
 
-        knightAnimator.SetFloat("Walk Speed", dir.magnitude);
+        //if (velocity.magnitude != 0)
+        //{
+        //    knightAnimator.SetFloat("Walk Speed", 1);
+        //}
+        //else
+        //{
+        //    knightAnimator.SetFloat("Walk Speed", 0);
+        //}
     }
 }
