2290b0cf4ea044bac178df9151a567980706fbb5
MattFiler <matthew.filer@hotmail.co.uk>
2019-11-22 20:07:43 +0000

    Debug stuff for working on camera

---
+++ StealthKnight/Assets/Scripts/DebugMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMove : MonoBehaviour
+{
+    public float MoveDist = 50.0f;
+    public bool MoveX = true;
+    public float MoveSpeed = 5.0f;
+    private bool DirectionFlipper = false;
+    private Vector3 StartPos;
+
+    private void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 TargetPos = StartPos;
+        if (DirectionFlipper)
+        {
+            if (MoveX) TargetPos += new Vector3(MoveDist, 0, 0);
+            else TargetPos += new Vector3(0, 0, MoveDist);
+        }
+        else
+        {
+            if (MoveX) TargetPos -= new Vector3(MoveDist, 0, 0);
+            else TargetPos -= new Vector3(0, 0, MoveDist);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, TargetPos, MoveSpeed * Time.deltaTime);
+
+        if (transform.position == TargetPos)
+        {
+            DirectionFlipper = !DirectionFlipper;
+        }
+    }
+}
---
+++ StealthKnight/Assets/Scripts/DebugPlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPlayerMove : MonoBehaviour
+{
+    public float MoveSpeed = 0.3f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(MoveSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(0, 0, MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(MoveSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0, 0, MoveSpeed);
+        }
+    }
+}
