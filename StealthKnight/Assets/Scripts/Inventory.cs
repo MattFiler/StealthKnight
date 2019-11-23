992a967f20032e1897092eeee14ab84dc5383c36
C <krzysztof2.chwiolka@live.uwe.ac.uk>
2019-11-23 22:12:53 +0100

    Basic Inventory and Items added

---
+++ StealthKnight/Assets/Scripts/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Item[] inventoryItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void addInventoryItem(Item item)
+    {
+        for(int i = 0; i < inventoryItems.Length; i++)
+        {
+            if(inventoryItems[i] != null)
+            {
+                inventoryItems[i] = item;
+            }
+        }
+    }
+
+    public void freeInventory()
+    {
+        for (int i = 0; i < inventoryItems.Length; i++)
+        {
+            inventoryItems[i] = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
