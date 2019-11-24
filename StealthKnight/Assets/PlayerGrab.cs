ab0ec99bc4df4bc77ad96264a70f1a4314bef791
Evan <evan2.diamond@live.uwe.ac.uk>
2019-11-24 21:08:43 +0000

    Added grab anim system

---
+++ StealthKnight/Assets/PlayerGrab.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour
+{
+    [SerializeField] private Animator knightAnimator;
+
+    [Range(0, 10)]
+    [SerializeField] private float grabSpeed = 1.0f;
+
+    private enum GrabHeight { LOW, MEDIUM, HIGH}
+    [SerializeField] GrabHeight grabHeight;
+
+    private Dictionary<GrabHeight, string> grabHeightDic = new Dictionary<GrabHeight, string>();
+
+    void Start()
+    {
+        knightAnimator.SetFloat("Grab Speed", grabSpeed);
+
+        grabHeightDic.Add(GrabHeight.LOW, "Low");
+        grabHeightDic.Add(GrabHeight.MEDIUM, "Medium");
+        grabHeightDic.Add(GrabHeight.HIGH, "High");
+    }
+
+    void Update()
+    {
+        if(grabSpeed != knightAnimator.GetFloat("Grab Speed"))
+        {
+            knightAnimator.SetFloat("Grab Speed", grabSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button4) || Input.GetKeyDown(KeyCode.Return))
+        {
+            knightAnimator.SetTrigger("Grab " + grabHeightDic[grabHeight]);
+        }
+    }
+}
