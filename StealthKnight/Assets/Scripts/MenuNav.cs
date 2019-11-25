0f9e023f3dc18f261513fe95411ba3c4c3fd5baa
Mike Ratcliffe <michael2.ratcliffe@live.uwe.ac.uk>
2019-11-25 18:33:38 +0000

    Added functioning menu with funky particles

---
+++ StealthKnight/Assets/Scripts/MenuNav.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNav : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+ 
+    }
+
+    public void QuitGame()
+    {
+       
+        Application.Quit(); 
+        Debug.Log("GET OUTTA HERE");
+    }
+}
