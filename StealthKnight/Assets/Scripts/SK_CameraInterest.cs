23f8973d9a71d55912065c962a3b396b5cf437e6
MattFiler <matthew.filer@hotmail.co.uk>
2019-11-25 11:55:07 +0000

    Camera object tracking base code

--- StealthKnight/Assets/Scripts/SK_CameraInterest.cs
+++ StealthKnight/Assets/Scripts/SK_CameraInterest.cs
@@ -5,5 +5,45 @@ using UnityEngine;
 /* An object that the camera can be interested in */
 public class SK_CameraInterest : MonoBehaviour
 {
-    // todo :)
+    private float DistToCamera = 0.0f;
+    private float DistToPlayer = 0.0f;
+    private bool IsInfrontCamera = false;
+    private bool IsEnabled = true;
+
+    private float TimeSinceLastTrack = 0.0f;
+
+    /* When initialised, add to our object tracker */
+    private void Start()
+    {
+        SK_CameraManager.Instance.AddInterest(this);
+    }
+
+    /* Update trackable object values */
+    private void Update()
+    {
+        TimeSinceLastTrack += Time.deltaTime;
+        if (TimeSinceLastTrack < SK_CameraManager.Instance.GetTickTime()) return;
+        TimeSinceLastTrack = 0.0f;
+
+        DistToCamera = Vector3.Distance(transform.position, SK_CameraManager.Instance.GetCamera().transform.position);
+        DistToPlayer = Vector3.Distance(transform.position, SK_CameraManager.Instance.GetPlayer().transform.position);
+
+        IsInfrontCamera = new Plane(SK_CameraManager.Instance.GetCamera().transform.forward, SK_CameraManager.Instance.GetCamera().transform.position).GetSide(transform.position);
+    }
+
+    /* Enable/disable tracking for this object */
+    public void Enable()
+    {
+        IsEnabled = true;
+    }
+    public void Disable()
+    {
+        IsEnabled = false;
+
+        DistToCamera = 0.0f;
+        DistToPlayer = 0.0f;
+        IsInfrontCamera = false;
+
+        TimeSinceLastTrack = 0.0f;
+    }
 }
--- StealthKnight/Assets/Scripts/SK_CameraManager.cs
+++ StealthKnight/Assets/Scripts/SK_CameraManager.cs
@@ -5,7 +5,41 @@ using UnityEngine;
 /* The manager in control of all objects the camera should be interested in */
 public class SK_CameraManager : MonoSingleton<SK_CameraManager>
 {
+    [SerializeField] private GameObject PlayerObject;
     private List<SK_CameraInterest> CameraInterests = new List<SK_CameraInterest>();
+
+    private SK_CameraController CameraController = null;
+    private GameObject CameraGameObject = null;
+
+    private float InterestTrackTickTime = 0.5f; //The seconds between interest track updates (lower time = better accuracy, but higher performance impact)
+
+    /* Get camera reference on start */
+    private void Start()
+    {
+        SetCamera(Camera.main.gameObject);
+    }
+
+    /* Get the player GameObject */
+    public GameObject GetPlayer()
+    {
+        return PlayerObject;
+    }
+
+    /* Get/set the camera GameObject */
+    public void SetCamera(GameObject cam)
+    {
+        CameraGameObject = cam;
+        CameraController = CameraGameObject.GetComponent<SK_CameraController>();
+        if (CameraController == null)
+        {
+            CameraGameObject.AddComponent<SK_CameraController>();
+            CameraController = CameraGameObject.GetComponent<SK_CameraController>();
+        }
+    }
+    public GameObject GetCamera()
+    {
+        return CameraGameObject;
+    }
 
     /* Add an interest */
     public void AddInterest(SK_CameraInterest interest)
@@ -23,5 +57,15 @@ public class SK_CameraManager : MonoSingleton<SK_CameraManager>
     public List<SK_CameraInterest> GetInterests()
     {
         return CameraInterests;
+    }
+
+    /* Set/get the update tick time for trackable objects */
+    public void SetTickTime(float tick)
+    {
+        InterestTrackTickTime = tick;
+    }
+    public float GetTickTime()
+    {
+        return InterestTrackTickTime;
     }
 }
