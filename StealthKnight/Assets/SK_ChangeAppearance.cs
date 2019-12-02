270b23fa7a3784bb365c92439b4f65869d013c1f
MattFiler <matthew.filer@hotmail.co.uk>
2019-12-02 16:57:31 +0000

    Added diversity

--- StealthKnight/Assets/SK_ChangeAppearance.cs
+++ StealthKnight/Assets/SK_ChangeAppearance.cs
@@ -14,7 +14,16 @@ public class SK_ChangeAppearance : MonoBehaviour
     void Start()
     {
         hair.material.SetColor("_Color", new Color(Random.Range(0.0f, 0.2f), Random.Range(0.0f, 0.2f), Random.Range(0.0f, 0.2f), 1.0f));
-        body.material.SetColor("_Color", new Color(Random.Range(0.7f, 1.0f), Random.Range(0.4f, 0.6f), Random.Range(0.4f, 0.6f), 1.0f));
+        if (Random.Range(0, 2) == 0)
+        {
+            float colour = Random.Range(0.4f, 0.6f);
+            body.material.SetColor("_Color", new Color(Random.Range(0.6f, 0.75f), colour, colour, 1.0f));
+        }
+        else
+        {
+            float colour = Random.Range(0.05f, 1.0f);
+            body.material.SetColor("_Color", new Color(colour, colour, colour, 1.0f));
+        }
         trousers.material.SetColor("_Color", new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f));
         shirt.material.SetColor("_Color", new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f));
     }
