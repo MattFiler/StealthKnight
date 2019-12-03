a771096a440e114816c83ae7e0fccdb0ea5e1720
Evan Diamond <evan2.diamond@live.uwe.ac.uk>
2019-12-03 22:01:56 +0000

    Added shimmer sound prefab

--- StealthKnight/Assets/Scripts/Item.cs
+++ StealthKnight/Assets/Scripts/Item.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+
 
 public class Item : MonoBehaviour
 {
@@ -18,10 +20,12 @@ public class Item : MonoBehaviour
 
     public GameObject prefabToDrop;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
