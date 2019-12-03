848c646973de2bd64ce509cfacd796305142a212
Evan Diamond <evan2.diamond@live.uwe.ac.uk>
2019-12-03 23:16:11 +0000

    Added portal sounds

---
+++ StealthKnight/Assets/PlayPortalSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class PlayPortalSound : MonoBehaviour
+{
+    StudioEventEmitter portalAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        portalAudio = GetComponent<StudioEventEmitter>();
+        portalAudio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
