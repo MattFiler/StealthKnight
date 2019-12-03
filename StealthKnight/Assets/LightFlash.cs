a87f07e4be22e3f26f6219dd193cd4f8cc0ecde0
MattFiler <matthew.filer@hotmail.co.uk>
2019-12-03 21:08:01 +0000

    Flashing warning light

---
+++ StealthKnight/Assets/LightFlash.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlash : MonoSingleton<LightFlash>
+{
+    private float TimeBetweenFlashes = 1.0f;
+    private float CurrentTimer = 0.0f;
+    private bool IsEnabled = false;
+    private Light LightComponent;
+    private void Start()
+    {
+        LightComponent = GetComponent<Light>();
+    }
+    void Update()
+    {
+        if (!IsEnabled) return;
+
+        CurrentTimer += Time.deltaTime;
+        if (CurrentTimer >= TimeBetweenFlashes)
+        {
+            LightComponent.enabled = !LightComponent.enabled;
+            CurrentTimer = 0.0f;
+        }
+    }
+
+    public void SetEnabled()
+    {
+        IsEnabled = true;
+    }
+}
